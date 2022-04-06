871e7938e8698bdcc24e61cf8a6965f585cbe582
JeremYnov <jeremy.richard@ynov.com>
2022-04-06 13:24:59 +0200

    issue with producer

--- object-detection-docker/producer/image-producer.py
+++ object-detection-docker/producer/image-producer.py
@@ -1,46 +1,44 @@
 from PIL import Image
-import io
 import os 
 from pathlib import Path
-from utils import (
-    set_logger, config_reader, acked)
-import zipfile
-import matplotlib.pyplot as plt 
-# from confluent_kafka import Producer
+from utils import (set_logger, config_reader, acked)
+from confluent_kafka import Producer
     
 PARENT_PATH = os.fspath(Path(__file__).parents[1])
+CONFIG_PATH = os.path.join(PARENT_PATH,"configurations","settings.ini")
+KAFKA_CONFIG_DICT = config_reader(CONFIG_PATH, "kafka")
+CRYPTO_TOPIC = config_reader(CONFIG_PATH, "app.settings")["topic_raw"]
+LOGGER = set_logger("producer_logger")
 
-CONFIG_PATH = os.path.join(
-    PARENT_PATH,
-    "configurations",
-    "settings.ini")
+from PIL import Image
+import numpy as np 
+from numpy import asarray
 
-KAFKA_CONFIG_DICT = config_reader(
-    CONFIG_PATH, "kafka")
-
-class ImageProducer: 
+class ImageProducer(): 
     def __init__(self) -> None:
         pass
 
-    def image_to_byte_array(self, image:Image):
-        imgByteArr = io.BytesIO()
-        image.save(imgByteArr, format=image.format)
-        imgByteArr = imgByteArr.getvalue()
-        print(imgByteArr)
-        return imgByteArr
+    def image_preprocessing(self, image_name:str):
+        img = Image.open(f'{PARENT_PATH}/{image_name}').resize((300,300))
+        img_np_array = asarray(img)
+        return img_np_array
+    
+    def image_decode(self, numpy_image_array):
+        pilImage =  Image.fromarray(numpy_image_array)
+        pilImage.show()
 
-    def read_images(self):
-        z = zipfile.ZipFile(f"{PARENT_PATH}/data.zip")
-        for i in range(len(z.namelist())):
-            file_in_zip = z.namelist()[i]
-            if (".jpg" in file_in_zip or ".JPG" in file_in_zip):
-                data = z.read(file_in_zip)
-                dataEnc = io.BytesIO(data)
-                print(dataEnc)
-                img = Image.open(dataEnc)
-
-    # def produce_image():
-    #     producer = Producer()
+    def produce_image(self):
+        producer = Producer(KAFKA_CONFIG_DICT)
+        np_array_image = self.image_preprocessing('street-image.jpg')
+        print(np_array_image)
+        try:
+            producer.produce(
+                topic=CRYPTO_TOPIC,
+                value=np_array_image,
+                callback=acked)
+            producer.poll(1)
+        except Exception as e:
+            print(f'The problem is : {e}')
 
 imageProducer = ImageProducer()
-imageProducer.read_images()
+imageProducer.image_preprocessing('street-image.jpg')
